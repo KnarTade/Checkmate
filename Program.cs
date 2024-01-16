2cf38d176f2e5c9a89fe2d2ebfd87b9e52c93b5c
Knar tad <knartadevosian@gmail.com>
2024-01-16 22:11:33 +0400

    Checkmate first project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Checkmate;
+
+internal class Program
+{
+    static void Main()
+    {
+        char[,] chessboard = BoardPrint.InitializeChessboard();
+        BoardPrint.PrintChessBoard(chessboard);
+        BoardPrint.SetPieces(chessboard);
+        BoardPrint.PrintChessBoard(chessboard);
+    }
+}
